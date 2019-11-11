0ae2efd5b40ed06f7d2f43276e1065a2f7551f8f
yumaloop <yumaloop@gmail.com>
2019-11-11 17:01:37 +0900

    update

---
+++ data/download.sh
@@ -0,0 +1,2 @@
+wget "https://20bn-data-packages.s3.eu-west-1.amazonaws.com/something-something/v2/20bn-something-something-v2-00?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJ2PXKOHYMBEGX4UA%2F20190924%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20190924T084224Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=6dc91dc9e659cc01e21cd0890e790957ea915df0c236a4b417cd7c8e66d282cb"
+
